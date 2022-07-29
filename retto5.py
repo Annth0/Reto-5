31c4b236bc9b90e59886f6f7af5607137bbe40ce
Anthonne <luangapa@gmail.com>
2022-07-29 13:01:46 -0500

    El reto 5 hecho :'v costó pero ajá

---
+++ retto5.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+rutaFileCsv = "https://github.com/luisguillermomolero/MisionTIC2022_2/blob/master/Modulo1_Python_MisionTIC2022_Main/Semana_5/Reto/movies.csv?raw=true"
+def listaPeliculas(rutaFileCsv:str)->str:
+    if rutaFileCsv.split('.')[-1]=='csv?raw=true':
+        try:
+            csv=pd.read_csv(rutaFileCsv)
+            subGrupoPeliculas = csv[['Country','Language','Gross Earnings']]
+            gananciaPaisLenguaje=subGrupoPeliculas.pivot_table(
+                index=['Country','Language'])
+            return gananciaPaisLenguaje.head(10)
+        except:
+            print('Error al leer el archivo de datos.')
+    else:
+        print('Extensión inválida.')
+    return
+print(listaPeliculas(rutaFileCsv))
